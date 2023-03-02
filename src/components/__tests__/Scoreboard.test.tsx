8dd6dd19328b83007098362027aa2c1c690ab3a8
unknown <rchen.utd@gmail.com>
2023-03-02 22:46:00 +1100

    rename Scoreboard tests

--- src/components/__tests__/Scoreboard.test.tsx
+++ src/components/__tests__/Scoreboard.test.tsx
@@ -2,22 +2,22 @@ import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Scoreboard from '../Scoreboard';
 
-describe('Renders Scoreboard component correctly', () => {
+describe('Scoreboard component tests', () => {
   beforeEach(() => {
     render(<Scoreboard />);
   });
 
-  test('Renders the Scoreboard component', () => {
+  test('Scoreboard component renders', () => {
     const scoreboardElement = screen.getByTestId('scoreboard');
     expect(scoreboardElement).toBeInTheDocument();
   });
 
-  test('Renders the Scoreboard component title', () => {
+  test('Scoreboard component renders default title', () => {
     const scoreboardElement = screen.getByTestId('scoreboard');
     expect(scoreboardElement).toHaveTextContent('Scoreboard');
   });
 
-  test('Renders 4 TeamScoreCards', () => {
+  test('Scoreboard component renders 4 TeamScoreCards components', () => {
     const teamScoreCards = screen.getAllByTestId('team-scorecard');
     expect(teamScoreCards).toHaveLength(4);
   });
