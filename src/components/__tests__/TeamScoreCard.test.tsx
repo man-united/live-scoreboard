94e75f2c94d5d73c1e4bf490ddbaff0c6f1a8735
unknown <rchen.utd@gmail.com>
2023-03-12 01:52:32 +1100

    rename test

--- src/components/__tests__/TeamScoreCard.test.tsx
+++ src/components/__tests__/TeamScoreCard.test.tsx
@@ -2,12 +2,12 @@ import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import TeamScoreCard from '../TeamScoreCard';
 
-describe('TeamScoreCard component tests', () => {
+describe('TeamScoreCard component', () => {
   beforeEach(() => {
     render(<TeamScoreCard />);
   });
 
-  test('TeamScoreCard component renders', () => {
+  it('should render', () => {
     const teamScoreCard = screen.getByTestId('team-scorecard');
     expect(teamScoreCard).toBeInTheDocument;
   });
